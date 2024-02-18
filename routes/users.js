f85b1fffa4603ab4dd6cb266db38ba88a6716263
chunxtan <chunxiaotan99@gmail.com>
2024-02-18 17:23:00 +0800

    add create endpoint for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var usersController = require('../controllers/users');
 var router = express.Router();
+
+// base path: /users
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Create a user profile
+router.post('/create', usersController.createUser);
 
 module.exports = router;
